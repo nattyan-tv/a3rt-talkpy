3a1653c9dfc1a65e055adfc39fbf5cf41135d209
NattyanTV <73095116+nattyan-tv@users.noreply.github.com>
2023-03-29 13:34:10 +0900

    fix: 使い方 miss

--- a3rt_talkpy/asyncclient.py
+++ a3rt_talkpy/asyncclient.py
@@ -10,37 +10,36 @@ class AsyncTalkClient:
         self.session = aiohttp.ClientSession() if session is None else session
 
     async def talk(self, query: str) -> Response:
-        async with self.session as session:
-            async with session.post(self.base_url, data={'apikey': self.apikey, 'query': query}) as res:
-                response = await res.json()
-                match response['status']:
-                    case 0:
-                        return Response(response=response)
-                    case 1000:
-                        raise ApiKeyIsNull
-                    case 1001:
-                        raise ApiKeyNotFound
-                    case 1002:
-                        raise DeletedAccount
-                    case 1003:
-                        raise TemporaryAccount
-                    case 1010:
-                        raise ServerNotFound
-                    case 1011:
-                        raise ServerParameterError
-                    case 1030:
-                        raise AccessDeny
-                    case 1400:
-                        raise BadRequest
-                    case 1404:
-                        raise NotFound
-                    case 1405:
-                        raise MethodNotAllowed
-                    case 1413:
-                        raise RequestEntityTooLong
-                    case 1500:
-                        raise InternalServerError
-                    case 2000:
-                        return Response(response=response)
-                    case _:
-                        raise UnknownError
+        async with self.session.post(self.base_url, data={'apikey': self.apikey, 'query': query}) as res:
+            response = await res.json()
+            match response['status']:
+                case 0:
+                    return Response(response=response)
+                case 1000:
+                    raise ApiKeyIsNull
+                case 1001:
+                    raise ApiKeyNotFound
+                case 1002:
+                    raise DeletedAccount
+                case 1003:
+                    raise TemporaryAccount
+                case 1010:
+                    raise ServerNotFound
+                case 1011:
+                    raise ServerParameterError
+                case 1030:
+                    raise AccessDeny
+                case 1400:
+                    raise BadRequest
+                case 1404:
+                    raise NotFound
+                case 1405:
+                    raise MethodNotAllowed
+                case 1413:
+                    raise RequestEntityTooLong
+                case 1500:
+                    raise InternalServerError
+                case 2000:
+                    return Response(response=response)
+                case _:
+                    raise UnknownError
