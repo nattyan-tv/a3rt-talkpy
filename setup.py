38e1fc370ed62e260b6056f8de3afb650686da63
NattyanTV <73095116+nattyan-tv@users.noreply.github.com>
2024-07-03 13:23:31 +0900

    add install requires (setup)

--- setup.py
+++ setup.py
@@ -7,6 +7,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 setuptools.setup(
     name=a3rt_talkpy.__name__,
     version=a3rt_talkpy.__version__,
+    install_requires=a3rt_talkpy.__install_requires__,
     author=a3rt_talkpy.__author__,
     description=a3rt_talkpy.__description__,
     long_description=long_description,
