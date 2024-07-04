60fae5c1cbba8ad73d3c3b84ff55a95edeb6bc51
nattyan-tv <73095116+nattyan-tv@users.noreply.github.com>
2024-07-04 14:47:03 +0900

    テストプログラムの改修

--- example_async.py
+++ example_async.py
@@ -4,11 +4,13 @@ import os
 from a3rt_talkpy import AsyncTalkClient
 
 API_KEY = os.environ.get("TALK_API_KEY")
+if API_KEY is None:
+    raise ValueError("API_KEY is not set")
 
 client = AsyncTalkClient(API_KEY)
 
 async def main():
-    query = "聞こえますか？"
+    query = "おはようございます"
     response = await client.talk(query)
     print(response.reply)
 
--- example_normal.py
+++ example_normal.py
@@ -3,11 +3,13 @@ import os
 from a3rt_talkpy import TalkClient
 
 API_KEY = os.environ.get("TALK_API_KEY")
+if API_KEY is None:
+    raise ValueError("API_KEY is not set")
 
 client = TalkClient(API_KEY)
 
 def main():
-    query = "聞こえますか？"
+    query = "おはようございます"
     response = client.talk(query)
     print(response.reply)
 
